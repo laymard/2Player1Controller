68c8b26178f2632a5f9e2e656e5d55f22071590a
laymard <aymard.laurent@gmail.com>
2019-05-28 23:17:47 -0400

    Init commit
    - add two player
    - add collision
    - support xbox one controller

---
+++ Assets/Prefabs/Player/HumanMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanMove : MonoBehaviour
+{
+    public float MaxSpeed;
+
+    private PlayerController m_PlayerController;
+    private Rigidbody m_Rigidbody;
+
+    private Vector3 MoveDirection;
+    private Vector3 LookDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_PlayerController = GetComponent<PlayerController>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateMoveDirection();
+        ApplySpeed();
+    }
+
+    private Vector2 GetInputDirection()
+    {
+        return m_PlayerController.GetInputDirection();
+    }
+
+    /// <summary>
+    /// Transform the input from pad to camera space
+    /// </summary>
+    private void UpdateMoveDirection()
+    {
+        Vector2 inputDirection = GetInputDirection();
+
+        MoveDirection.Set(inputDirection.x, 0.0f, inputDirection.y);
+
+        float yCameraAngle = Camera.main.transform.rotation.eulerAngles.y;
+
+        MoveDirection = Quaternion.AngleAxis(yCameraAngle, Vector3.up) * MoveDirection;
+    }
+
+    private void ApplySpeed()
+    {
+        /// apply velocity
+        Vector3 velocity = MoveDirection * MaxSpeed;
+        m_Rigidbody.velocity = velocity;
+
+        /// change orientation only if player is moving
+        if (MoveDirection.sqrMagnitude > 0.0f)
+        {
+            Quaternion rotation = Quaternion.LookRotation(MoveDirection, Vector3.up);
+            transform.rotation = rotation;
+        }
+    }
+}
---
+++ Assets/Prefabs/Player/PlayerController.cs
@@ -0,0 +1,81 @@
+﻿
+using UnityEngine;
+
+public enum EPlayerID
+{
+    EPlayer1,
+    EPlayer2
+}
+public class PlayerController : MonoBehaviour
+{
+    //public properties
+    public EPlayerID PlayerID;
+
+    public Material[] materials;
+
+    private int index;
+    private MeshRenderer _meshRenderer;
+
+    private Vector2 m_inputDirection;
+    private string inputVerticalDirection;
+    private string inputHorizontalDirection;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        index = 0;
+        _meshRenderer = GetComponent<MeshRenderer>();
+
+        // input
+        inputVerticalDirection = PlayerID == EPlayerID.EPlayer1 ? "Player1_Vertical" : "Player2_Vertical";
+        inputHorizontalDirection = PlayerID == EPlayerID.EPlayer1 ? "Player1_Horizontal" : "Player2_Horizontal";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsActionButtonDown())
+        {
+            SwitchMaterial();
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        UpdateInputDirection();
+    }
+
+    private bool IsActionButtonDown()
+    {
+        string ButtonName = PlayerID == EPlayerID.EPlayer1 ? "Player1_Action" : "Player2_Action";
+
+        return Input.GetButtonDown(ButtonName);
+    }
+
+    private void SwitchMaterial()
+    {
+        int nextIndex;
+        nextIndex = (index + 1) % materials.Length;
+
+        Material nextMaterial = materials[nextIndex];
+        _meshRenderer.material = nextMaterial;
+
+        index = nextIndex;
+    }
+
+    private void UpdateInputDirection()
+    {
+        float verticalDirection = Input.GetAxis(inputVerticalDirection);
+        float horizontalDirection = Input.GetAxis(inputHorizontalDirection);
+
+        /// xbox controller specific
+        verticalDirection *= -1.0f;
+
+        m_inputDirection.Set(horizontalDirection, verticalDirection);
+    }
+
+    public Vector2 GetInputDirection()
+    {
+        return m_inputDirection;
+    }
+}
